07fbfa7af6340d768cea40a6dfb58e0542ca9dbd
Inquisitor-201 <1365605830@qq.com>
2022-11-25 19:47:18 +0800

    add machine module for elfloader

--- tools/elfloader/src/main.rs
+++ tools/elfloader/src/main.rs
@@ -1,10 +1,17 @@
 #![no_std]
 #![no_main]
-use core::arch::global_asm;
+mod machine;
 
+use core::arch::global_asm;
 use core::panic::PanicInfo;
 
 #[panic_handler]
 pub fn panic_handler(_info: &PanicInfo) -> ! {
     loop { }
+}
+
+#[no_mangle]
+pub fn main(hard_id: i64, bootloader_dtb: *mut u64) -> ! {
+    println!("hard_id = {:#x?}, bootloader_dtb = {:#x?}", hard_id, bootloader_dtb);
+    panic!();
 }
