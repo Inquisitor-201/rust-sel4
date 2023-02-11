13e2dccfebc905c2cc2fa4769645f43ef8815d90
Inquisitor-201 <1365605830@qq.com>
2023-02-11 12:31:30 +0800

    fix _start problem in user_app

--- apps/src/lib.rs
+++ apps/src/lib.rs
@@ -3,24 +3,26 @@
 #![feature(panic_info_message)]
 #![feature(alloc_error_handler)]
 
-extern crate alloc;
+use core::arch::global_asm;
+
+// extern crate alloc;
 
 mod lang_items;
-use alloc::vec::Vec;
-use buddy_system_allocator::LockedHeap;
+// use buddy_system_allocator::LockedHeap;
 
-const USER_HEAP_SIZE: usize = 32768;
+// const USER_HEAP_SIZE: usize = 32768;
 
-#[global_allocator]
-static HEAP: LockedHeap<32> = LockedHeap::empty();
+// #[global_allocator]
+// static HEAP: LockedHeap<32> = LockedHeap::empty();
 
-#[alloc_error_handler]
-pub fn handle_alloc_error(layout: core::alloc::Layout) -> ! {
-    panic!("Heap allocation error, layout = {:?}", layout);
-}
+// #[alloc_error_handler]
+// pub fn handle_alloc_error(layout: core::alloc::Layout) -> ! {
+//     panic!("Heap allocation error, layout = {:?}", layout);
+// }
 
-static mut HEAP_SPACE: [u8; USER_HEAP_SIZE] = [0; USER_HEAP_SIZE];
+// static mut HEAP_SPACE: [u8; USER_HEAP_SIZE] = [0; USER_HEAP_SIZE];
 
+#[no_mangle]
 pub fn sel4_start_root(bootinfo: usize) -> usize {
     return bootinfo;
 }
@@ -30,3 +32,5 @@ pub fn sel4_start_root(bootinfo: usize) -> usize {
 fn main(_argc: usize, _argv: &[&str]) -> i32 {
     panic!("Cannot find main!");
 }
+
+global_asm!(include_str!("sel4_crt0.S"));
